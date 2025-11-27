8c4f96a12117ed36e290c1c8202c9b540e651436
Emyli Martinez <emyli_29@hotmail.com>
2025-11-26 19:39:44 -0600

    conectar estilos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",   // Busca en la carpeta app
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // Busca en components
   ],
   theme: {
     extend: {},
